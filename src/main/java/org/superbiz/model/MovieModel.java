fde598badb1a0605238a4896104d4c7d800df74e
Roberto Cortez <radcortez@yahoo.com>
2018-05-31 13:36:44 +0100

    Added model and resource generation.

---
+++ src/main/java/org/superbiz/model/MovieModel.java
@@ -0,0 +1,15 @@
+package org.superbiz.model;
+
+import org.tomitribe.api.Model;
+import org.tomitribe.api.Resource;
+
+@Resource
+class MovieModel {
+    @Model(id = true)
+    private String id;
+    private String director;
+    private String title;
+    private int year;
+    private String genre;
+    private int rating;
+}
