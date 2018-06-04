c55990d0813df30b65abbcb95179142e00a22de8
Roberto Cortez <radcortez@yahoo.com>
2018-06-04 16:10:19 +0100

    Removed trapease plugin.

--- src/main/java/org/superbiz/moviefun/rest/LoadDataResource.java
+++ src/main/java/org/superbiz/moviefun/rest/LoadDataResource.java
@@ -16,7 +16,7 @@
  */
 package org.superbiz.moviefun.rest;
 
-import org.superbiz.moviefun.MovieEntity;
+import org.superbiz.moviefun.Movie;
 import org.superbiz.moviefun.MoviesBean;
 
 import javax.ejb.EJB;
@@ -30,13 +30,13 @@ public class LoadDataResource {
 
     @POST
     public void load() {
-        moviesBean.addMovie(new MovieEntity("Wedding Crashers", "David Dobkin", "Comedy", 7, 2005));
-        moviesBean.addMovie(new MovieEntity("Starsky & Hutch", "Todd Phillips", "Action", 6, 2004));
-        moviesBean.addMovie(new MovieEntity("Shanghai Knights", "David Dobkin", "Action", 6, 2003));
-        moviesBean.addMovie(new MovieEntity("I-Spy", "Betty Thomas", "Adventure", 5, 2002));
-        moviesBean.addMovie(new MovieEntity("The Royal Tenenbaums", "Wes Anderson", "Comedy", 8, 2001));
-        moviesBean.addMovie(new MovieEntity("Zoolander", "Ben Stiller", "Comedy", 6, 2001));
-        moviesBean.addMovie(new MovieEntity("Shanghai Noon", "Tom Dey", "Comedy", 7, 2000));
+        moviesBean.addMovie(new Movie("Wedding Crashers", "David Dobkin", "Comedy", 7, 2005));
+        moviesBean.addMovie(new Movie("Starsky & Hutch", "Todd Phillips", "Action", 6, 2004));
+        moviesBean.addMovie(new Movie("Shanghai Knights", "David Dobkin", "Action", 6, 2003));
+        moviesBean.addMovie(new Movie("I-Spy", "Betty Thomas", "Adventure", 5, 2002));
+        moviesBean.addMovie(new Movie("The Royal Tenenbaums", "Wes Anderson", "Comedy", 8, 2001));
+        moviesBean.addMovie(new Movie("Zoolander", "Ben Stiller", "Comedy", 6, 2001));
+        moviesBean.addMovie(new Movie("Shanghai Noon", "Tom Dey", "Comedy", 7, 2000));
     }
 
 }
